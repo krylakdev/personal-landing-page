369cfa1a8afa2e4bc015bd7bb4e4b35a2d96c7ad
Krystian Grzelak <k.grzelak.dev@gmail.com>
2024-10-09 13:22:43 +0200

    chore: remove zone.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'personal-landing-page';

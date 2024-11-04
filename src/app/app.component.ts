40c3b9fe25f8de9f048ff91ec201889a95aa9f21
Krystian Grzelak <k.grzelak.dev@gmail.com>
2024-11-04 13:18:21 +0100

    feat: insert to app.component section-tech-stack component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SectionContainerComponent } from '@shared/components';
-import { FooterComponent, HeroComponent, SectionAboutMeComponent } from '@components';
+import { FooterComponent, HeroComponent, SectionAboutMeComponent, SectionTechStackComponent } from '@components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SectionContainerComponent, HeroComponent, FooterComponent, SectionAboutMeComponent],
+  imports: [
+    SectionContainerComponent,
+    HeroComponent,
+    FooterComponent,
+    SectionAboutMeComponent,
+    SectionTechStackComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,3 +1,4 @@
 export * from './hero/hero.component';
 export * from './section-about-me/section-about-me.component';
+export * from './section-tech-stack/section-tech-stack.component';
 export * from './footer/footer.component';

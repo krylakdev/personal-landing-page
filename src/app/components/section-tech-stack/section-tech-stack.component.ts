75035c259ff242280fbe0370377f47de1eaa70f7
Krystian Grzelak <k.grzelak.dev@gmail.com>
2024-11-05 12:37:47 +0100

    feat: create base tech card styles

--- src/app/components/section-tech-stack/section-tech-stack.component.ts
+++ src/app/components/section-tech-stack/section-tech-stack.component.ts
@@ -1,15 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ITechCard } from '@shared/models';
 import { techCardList } from '@shared/constants';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-section-tech-stack',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './section-tech-stack.component.html',
   styleUrl: './section-tech-stack.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SectionTechStackComponent {
-  experienceCardList: ITechCard[] = techCardList;
+  techCardList: ITechCard[] = techCardList;
 }
--- src/app/shared/constants/tech-card-list.constants.ts
+++ src/app/shared/constants/tech-card-list.constants.ts
@@ -1,3 +1,8 @@
 import { ITechCard } from '@shared/models';
 
-export const techCardList: ITechCard[] = [];
+export const techCardList: ITechCard[] = [
+  {
+    name: 'HTML5',
+    imgPath: 'images/icons/html5-color.svg',
+  },
+];

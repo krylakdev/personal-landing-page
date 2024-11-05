2dcd8aea60d90e6c2c45a674383652fe4d51d609
Krystian Grzelak <k.grzelak.dev@gmail.com>
2024-11-05 12:59:31 +0100

    feat: update tech card list

--- src/app/shared/constants/tech-card-list.constants.ts
+++ src/app/shared/constants/tech-card-list.constants.ts
@@ -5,4 +5,84 @@ export const techCardList: ITechCard[] = [
     name: 'HTML5',
     imgPath: 'images/icons/html5-color.svg',
   },
+  {
+    name: 'CSS3',
+    imgPath: 'images/icons/css3-color.svg',
+  },
+  {
+    name: 'JavaScript',
+    imgPath: 'images/icons/javascript-color.svg',
+  },
+  {
+    name: 'TypeScript',
+    imgPath: 'images/icons/typescript-color.svg',
+  },
+  {
+    name: 'Angular',
+    imgPath: 'images/icons/angular-color.svg',
+  },
+  {
+    name: 'Nx',
+    imgPath: 'images/icons/nx-color.svg',
+  },
+  {
+    name: 'Sass',
+    imgPath: 'images/icons/sass-color.svg',
+  },
+  {
+    name: 'Bootstrap',
+    imgPath: 'images/icons/bootstrap-color.svg',
+  },
+  {
+    name: 'Tailwind CSS',
+    imgPath: 'images/icons/tailwindcss-color.svg',
+  },
+  {
+    name: 'Prime NG',
+    imgPath: 'images/icons/primeng-color.svg',
+  },
+  {
+    name: 'RXJS',
+    imgPath: 'images/icons/reactivex-color.svg',
+  },
+  {
+    name: 'NgRx',
+    imgPath: 'images/icons/ngrx-color.svg',
+  },
+  {
+    name: 'Storybook',
+    imgPath: 'images/icons/storybook-color.svg',
+  },
+  {
+    name: 'Jest',
+    imgPath: 'images/icons/jest-color.svg',
+  },
+  {
+    name: 'Jasmine',
+    imgPath: 'images/icons/jasmine-color.svg',
+  },
+  {
+    name: 'Testing Library',
+    imgPath: 'images/icons/testinglibrary-color.svg',
+  },
+  {
+    name: 'Git',
+    imgPath: 'images/icons/git-color.svg',
+  },
+  {
+    name: 'GitHub',
+    imgPath: 'images/icons/github-color.svg',
+  },
+  {
+    name: 'GitLab',
+    imgPath: 'images/icons/gitlab-color.svg',
+  },
+  {
+    name: 'NestJS',
+    imgPath: 'images/icons/nestjs-color.svg',
+  },
+  {
+    name: 'Keycloak Templates',
+    imgPath: 'images/icons/keycloak-color.svg',
+  },
 ];
